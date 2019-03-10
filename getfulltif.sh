4c3a2eaebb5df11671daf56585dbd5d571103ae4
zxd <izayoi5776@gmail.com>
2019-03-11 00:49:59 +0900

    use local function

--- getfulltif.sh
+++ getfulltif.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
 # 3902-r00223-0
-PART1=http://codh.rois.ac.jp/north-china-railway/iiif/original/
-PART2=.tif/full/full/0/default.jpg
+function download(){
+	PART1=http://codh.rois.ac.jp/north-china-railway/iiif/original/
+	PART2=.tif/full/full/0/default.jpg
+	FN=./images/$1.jpg
+	if [ -f ${FN} ]; then
+	  echo skip ${FN}
+	else
+	  curl ${PART1}$1${PART2} --output ${FN}
+	fi
+}
 
-for i in $(cat id.txt); do
-  FN=${i}.jpg
-  if [ -f ${FN} ]; then
-    echo skip ${FN}
-  else
-    curl ${PART1}$i${PART2} --output ${FN}
-    #sleep 1
-  fi
-done
+export -f download
+cat id.txt | xargs -n 1 -P 10 -I@ bash -c 'download @'
